248679a3d98fdc0b2e448584b333d80329b3377a
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-03-26 20:14:01 -0300

    grouped tests

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,26 @@
 const sum = require("./sum")
 
-test("deve somar dois numeros", ()=>{
-    expect(sum(5, 3)).toBe(8);
+
+describe("Testes de soma", ()=>{
+    test("deve somar dois numeros", ()=>{
+        expect(sum(5, 3)).toBe(8);
+    })
+
+    test("deve verificar o resultado incorreto de uma soma", ()=>{
+        expect(sum(5, 3)).not.toBe(7);
+    })
+})
+
+
+
+// podemos subistituir a palavra test por it, tem o mesmo funcionamento.
+describe("Testes de soma2", ()=>{
+    it("deve somar dois numeros", ()=>{
+        expect(sum(5, 3)).toBe(8);
+    })
+
+    it("deve verificar o resultado incorreto de uma soma", ()=>{
+        expect(sum(5, 3)).not.toBe(7);
+    })
 })
 
