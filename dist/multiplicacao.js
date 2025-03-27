d325f09a90f9295f6bf2abf673526831d429e52a
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-03-26 21:40:36 -0300

    Removido a pasta dist

--- dist/multiplicacao.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.multiplicacao = multiplicacao;
-function multiplicacao(num1, num2) {
-    return num1 * num2;
-}
--- dist/subtracao.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.subtracao = subtracao;
-function subtracao(num1, num2) {
-    return num1 - num2;
-}
